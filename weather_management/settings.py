b6824f5ae0690a7d12799bf2656b660c8700f7d1
winmar11 <delrosariomarwin343@gmail.com>
2026-02-11 14:10:35 +0800

    Use Postgres on Render and auto-migrate

--- weather_management/settings.py
+++ weather_management/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 try:
     from dotenv import load_dotenv
 except ImportError:
@@ -94,10 +96,10 @@ WSGI_APPLICATION = 'weather_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
