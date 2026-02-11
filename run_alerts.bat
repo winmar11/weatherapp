341deaaea5112bc3b4591bd9921ee954d6a9f2e7
winmar11 <delrosariomarwin343@gmail.com>
2026-02-11 14:30:54 +0800

    Warn when running local alerts

--- run_alerts.bat
+++ run_alerts.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d c:\Users\Jasmine\Downloads\weather!-20260210T150019Z-1-001\weather!
+echo NOTE: This runs against the local database and may differ from Render production.
 python manage.py process_alerts
   
